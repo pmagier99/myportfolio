116826e82c54260aab312edf159fafaf8bb4c1ec
pmagier99 <pmagier99@gmail.com>
2023-01-24 11:24:45 +0000

    creating About

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import NavBar from './components/NavBar'
 import Intro from './components/Intro'
 import Portfolio from "./components/Portfolio";
 import Offers from "./components/Offers";
+import About from "./components/About";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Intro />
         <Portfolio />
         <Offers />
+        <About />
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,66 @@
+import styled from 'styled-components'
+import {FaLinkedin} from 'react-icons/fa'
+import {FaGithub} from 'react-icons/fa'
+import {FaEnvelope} from 'react-icons/fa'
+
+
+const StyledAbout = styled.div`
+  margin-top: 300px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 100px;
+  
+  div:nth-child(2){
+    place-self: center;
+    align-self: start;
+    text-align: center;
+  }
+  
+  .media{
+    display: flex;
+    justify-content: flex-start;
+    flex-flow: row nowrap;
+    font-size: 2rem;
+    svg{margin: 20px 20px 0 0}
+  }
+  
+  h2{margin: 10px 0}
+  span{color: #F8CC34}
+  p{text-align: justify}
+  ul{
+    list-style-type: none;
+    li{
+      margin: 20px 0;
+    }
+  }
+`
+
+function About(){
+    return(
+        <>
+            <StyledAbout>
+                <div>
+                    <h2>Piotr <span>Magier</span></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget purus leo. Nam mi elit, efficitur pretium sodales a, tempus nec ipsum. Sed lorem lectus, molestie sed auctor sed, aliquam a nulla. Nunc feugiat felis quis dui imperdiet, ut dapibus velit tincidunt. </p>
+                    <div className={"media"}>
+                        <FaGithub />
+                        <FaLinkedin />
+                        <FaEnvelope />
+                    </div>
+                </div>
+
+                <div>
+                    <h2>Quick <span>links</span></h2>
+                    <ul>
+                        <a href={"#"}><li>about</li></a>
+                        <a href={"#"}><li>portfolio</li></a>
+                        <a href={"#"}><li>contact</li></a>
+                    </ul>
+                </div>
+                <p>&copy; Piotr Magier 2023</p>
+            </StyledAbout>
+        </>
+    )
+}
+
+export default About
